456dc6d2e8f33b95a9a16d448d2e470bc4beaaf5
lomin <happylomin@googlemail.com>
2015-03-01 16:05:12 +0100

    - removed calculator from component Server. - reduced on level of nested keys.

--- src/de/otto/tesla/stateful/serving.clj
+++ src/de/otto/tesla/stateful/serving.clj
@@ -7,11 +7,11 @@
 ;; The serving component is the frontend of the system.
 ;; It accepts requests and returns the data to be used by consuming systems.
 ;; For the moment a simple, blocking implementation with an embedded jetty is chosen.
-(defrecord Server [config calculator]
+(defrecord Server [config]
   component/Lifecycle
   (start [self]
     (log/info "-> starting server")
-    (let [port (Integer. (get-in self [:config :config :server-port]))
+    (let [port (Integer. (get-in config [:config :server-port]))
           all-routes (rts/routes (:routes self))
           server (jetty/run-jetty all-routes {:port port :join? false})]
       (.setGracefulShutdown server 100)
