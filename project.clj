260d24c22dc7281599073e92b03274d44785c6f1
Christian Stamm <christian.stamm@otto.de>
2015-05-29 22:23:16 +0200

    cleaned up dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.1.11"
+(defproject de.otto/tesla-microservice "0.1.12"
             :description "basic microservice."
             :url "https://github.com/otto-de/tesla-microservice"
             :license { :name "Apache License 2.0" 
@@ -6,34 +6,31 @@
             :scm { :name "git"
  	           :url "https://github.com/otto-de/tesla-microservice"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [com.stuartsierra/component "0.2.2"]
-                           [org.clojure/core.cache "0.6.4"]
+                           [com.stuartsierra/component "0.2.3"]
                            [clojurewerkz/propertied "1.2.0"]
-                           [org.clojure/data.json "0.2.5"]
+                           [org.clojure/data.json "0.2.6"]
                            [beckon "0.1.1"]
                            [overtone/at-at "1.2.0"]
                            [environ "1.0.0"]
-                           [clj-time "0.8.0"]
+                           [clj-time "0.9.0"]
 
                            [de.otto/status "0.1.0"]
 
                            ;; io
-                           [ring "1.3.1"]
-                           [compojure "1.2.0"]
+                           [ring "1.3.2"]
+                           [compojure "1.3.4"]
                            [hiccup "1.0.5"]
-                           [metrics-clojure "2.3.0"]
-                           [metrics-clojure-graphite "2.3.0"]
+                           [metrics-clojure "2.5.1"]
+                           [metrics-clojure-graphite "2.5.1"]
 
                            ;; logging
-                           [org.clojure/tools.logging "0.3.0"]
-                           [org.slf4j/slf4j-api "1.7.7"]
-                           [ch.qos.logback/logback-core "1.1.2"]
-                           [ch.qos.logback/logback-classic "1.1.2"]
-                           [net.logstash.logback/logstash-logback-encoder "3.4"]
+                           [org.clojure/tools.logging "0.3.1"]
+                           [org.slf4j/slf4j-api "1.7.12"]
+                           [ch.qos.logback/logback-core "1.1.3"]
+                           [ch.qos.logback/logback-classic "1.1.3"]
 
                            ;; testing
-                           [ring-mock "0.1.5"]
-                           [http-kit.fake "0.2.1"]]
+                           [ring-mock "0.1.5"]]
 
             :exclusions [org.clojure/clojure
                          org.slf4j/slf4j-nop
@@ -41,8 +38,7 @@
                          log4j
                          commons-logging/commons-logging]
 
-            :plugins [[lein-marginalia "0.8.0"]
-                      [lein-environ "1.0.0"]]
+            :plugins [[lein-environ "1.0.0"]]
             :main ^:skip-aot de.otto.tesla.example.example-system
             :aot [de.otto.tesla.util.escapingmessageconverter]
             :clean-targets [:target-path :compile-path "target"]
