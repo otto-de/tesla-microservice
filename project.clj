86290d574f64a7e163cf3c2d441bf821d2c65842
Christian Stamm <christian.stamm@otto.de>
2015-05-31 19:19:12 +0200

    removed example as it was moved to github.com/otto-de/tesla-examples

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
 (defproject de.otto/tesla-microservice "0.1.12"
             :description "basic microservice."
             :url "https://github.com/otto-de/tesla-microservice"
-            :license { :name "Apache License 2.0" 
-                       :url "http://www.apache.org/license/LICENSE-2.0.html"}
-            :scm { :name "git"
- 	           :url "https://github.com/otto-de/tesla-microservice"}
+            :license {:name "Apache License 2.0"
+                      :url  "http://www.apache.org/license/LICENSE-2.0.html"}
+            :scm {:name "git"
+                  :url  "https://github.com/otto-de/tesla-microservice"}
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [com.stuartsierra/component "0.2.3"]
                            [clojurewerkz/propertied "1.2.0"]
                            [org.clojure/data.json "0.2.6"]
                            [beckon "0.1.1"]
-                           [overtone/at-at "1.2.0"]
                            [environ "1.0.0"]
                            [clj-time "0.9.0"]
 
@@ -39,20 +38,18 @@
                          commons-logging/commons-logging]
 
             :plugins [[lein-environ "1.0.0"]]
-            :main ^:skip-aot de.otto.tesla.example.example-system
             :aot [de.otto.tesla.util.escapingmessageconverter]
             :clean-targets [:target-path :compile-path "target"]
-            :source-paths ["src" "example/src"]
+            :source-paths ["src"]
             :java-source-paths ["src/java"]
             :test-selectors {:default     (constantly true)
                              :integration :integration
                              :unit        :unit
                              :all         (constantly true)}
-            :profiles {:test {:aot [de.otto.tesla.util.escapingmessageconverter]
-                              :env {:metering-reporter "console"
-                                    :import-products   "false"
-                                    :server-port       "9991"
-                                    :cache-dir         "/tmp"}}
-                       :meta {:env {:app-name :tesla-meta}}
+            :profiles {:test    {:aot [de.otto.tesla.util.escapingmessageconverter]
+                                 :env {:metering-reporter "console"
+                                       :server-port       "9991"
+                                       :cache-dir         "/tmp"}}
+                       :meta    {:env {:app-name :tesla-meta}}
                        :uberjar {:aot :all}}
-            :test-paths ["test" "test-resources" "example/test"])
+            :test-paths ["test" "test-resources"])
