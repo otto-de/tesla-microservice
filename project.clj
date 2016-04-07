b4358e86b42e7c9cd16aacdd7a82c1ce0c3d449f
Torsten Mangner <tmangner@inoxio.de>
2016-04-07 10:42:53 +0200

    version bump to 0.1.30

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.1.29"
+(defproject de.otto/tesla-microservice "0.1.30"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
