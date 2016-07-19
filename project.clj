75043d4eb244510e62247e227424683a80222b29
tesla <tesla@dv.otto.de>
2016-07-19 13:01:09 +0200

    dependency update

--- project.clj
+++ project.clj
@@ -6,24 +6,21 @@
   :scm {:name "git"
         :url  "https://github.com/otto-de/tesla-microservice"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [clojurewerkz/propertied "1.2.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [beckon "0.1.1"]
-                 [environ "1.0.2"]
-                 [clj-time "0.11.0"]
-                 [overtone/at-at "1.2.0"]
+                 [org.clojure/tools.logging "0.3.1"]
 
                  [de.otto/status "0.1.0"]
 
-                 ;; io
-                 [ring/ring-core "1.4.0"]
-
-                 [compojure "1.4.0"]
-                 [metrics-clojure "2.6.1"]
-                 [metrics-clojure-graphite "2.6.1"]
-
-                 [org.clojure/tools.logging "0.3.1"]]
+                 [beckon "0.1.1"]
+                 [clj-time "0.12.0"]
+                 [clojurewerkz/propertied "1.2.0"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [compojure "1.5.1"]
+                 [environ "1.0.3"]
+                 [metrics-clojure "2.7.0"]
+                 [metrics-clojure-graphite "2.7.0"]
+                 [overtone/at-at "1.2.0"]
+                 [ring/ring-core "1.5.0"]]
 
   :exclusions [org.clojure/clojure
                org.slf4j/slf4j-nop
@@ -37,9 +34,9 @@
                    :all         (constantly true)}
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                      [org.slf4j/slf4j-api "1.7.14"]
-                                      [ch.qos.logback/logback-core "1.1.3"]
-                                      [ch.qos.logback/logback-classic "1.1.3"]
+                                      [org.slf4j/slf4j-api "1.7.21"]
+                                      [ch.qos.logback/logback-core "1.1.7"]
+                                      [ch.qos.logback/logback-classic "1.1.7"]
                                       [ring-mock "0.1.5"]]
                        :plugins      [[lein-ancient "0.6.8"]
                                       [lein-environ "1.0.2"]]}}
