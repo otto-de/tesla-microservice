c97c20cb2c356f768d87eb8e0a7f22af433f276d
Kai Brandes <kai.brandes@otto.de>
2016-10-21 14:36:10 +0200

    lein-release plugin: bumped version from 0.4.0 to 0.4.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.4.0"
+(defproject de.otto/tesla-microservice "0.4.1-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
