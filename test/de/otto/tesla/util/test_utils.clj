64b7b28893d93a8cc22a545864f21ee810c2c6c5
Torsten Mangner <tmangner@inoxio.de>
2016-10-26 16:19:45 +0200

    Add docstring for new `eventually` assertion

--- test/de/otto/tesla/util/test_utils.clj
+++ test/de/otto/tesla/util/test_utils.clj
@@ -3,14 +3,26 @@
             [com.stuartsierra.component :as comp]
             [ring.mock.request :as mock]))
 
-(defmacro eventually [body & {:keys [timeout interval]
+(defmacro eventually 
+  "Generic assertion macro, that waits for a predicate test to become true.
+  'form' is a predicate test, that clojure.test/is can understand
+  'timeout': optional; in ms; how long to wait in total (defaults to 5000)
+  'interval' optional; in ms; how long to pause between tries (defaults to 10)
+  
+  Example:
+  Since this will fail half of the time ...
+    (is (= 1 (rand-int 2)))
+  
+  ... use this:
+    (eventually (= 1 (rand-int 2)))"
+  [form & {:keys [timeout interval]
                               :or   {timeout  5000
                                      interval 10}}]
   `(let [start-time# (System/currentTimeMillis)]
      (loop []
        (let [last-stats# (atom nil)
              pass?# (with-redefs [clojure.test/do-report (fn [s#] (reset! last-stats# s#))]
-                      (clojure.test/is ~body))
+                      (clojure.test/is ~form))
              took-too-long?# (> (- (System/currentTimeMillis) start-time#) ~timeout)]
          (if (or pass?# took-too-long?#)
            (clojure.test/do-report @last-stats#)
