cb0dd0b23bfd3d80f784a791a67f4e8dda9c0cf4
Kai Brandes <kai.brandes@otto.de>
2016-10-27 11:18:45 +0200

    lein-release plugin: preparing 0.5.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.5.2-SNAPSHOT"
+(defproject de.otto/tesla-microservice "0.5.2"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
