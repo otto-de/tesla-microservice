8ad669367d9ee29e0cf2163a8bd95008adfa5791
Daniel Rolfes <daniel.rolfes@otto.de>
2017-07-12 17:44:14 +0200

    dr/ts| use new status version to return timeout for long running status functions

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
         :url  "https://github.com/otto-de/tesla-microservice"}
   :dependencies [[org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.4.0"]
-                 [de.otto/status "0.1.0"]
+                 [de.otto/status "0.1.3"]
                  [iapetos "0.1.6"]
                  [metrics-clojure "2.9.0"]
                  [beckon "0.1.1"]
