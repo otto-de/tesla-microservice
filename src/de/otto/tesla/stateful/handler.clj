0f5a241990c8c1b9c1a47fac900eb7677aa117d3
Torsten Mangner <tmangner@inoxio.de>
2017-09-04 16:04:30 +0200

    Remove leftovers from goo-extraction

--- src/de/otto/tesla/stateful/handler.clj
+++ src/de/otto/tesla/stateful/handler.clj
@@ -121,9 +121,6 @@
   component/Lifecycle
   (start [self]
     (log/info "-> starting Handler")
-    (let [http-labels [:path :method :rc]]
-      (goo/register-counter! :http/calls-total {:labels http-labels})
-      (goo/register-histogram! :http/duration-in-s {:labels http-labels :buckets [0.05 0.1 0.15 0.2]}))
     (assoc self
       :reporting-base-path (get-in config [:config :handler :reporting-base-path] ["serving" "requests"])
       :reporting-time-window-in-min (get-in config [:config :handler :reporting-time-window-in-min] 1)
