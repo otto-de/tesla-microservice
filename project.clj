7bdec05a1c2c1e3c00fc93f8d10ef08fef21d2a1
Carl Düvel <carl.duevel@otto.de>
2017-09-04 15:29:55 +0200

    CD| bump version as TTS did release but did not push

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.11.8-SNAPSHOT"
+(defproject de.otto/tesla-microservice "0.11.9-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
