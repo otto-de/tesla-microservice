3fdf2aff767b13eead972e7a9f0409221d04e3dd
Carl Düvel <carl.duevel@otto.de>
2017-12-04 16:06:09 +0100

    lein-release plugin: bumped version from 0.11.15 to 0.11.16-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.11.15"
+(defproject de.otto/tesla-microservice "0.11.16-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
