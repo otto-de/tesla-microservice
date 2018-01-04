3209576b99fb34eb3669d2a643db13eb045f0bc8
Carl Düvel <carl.duevel@otto.de>
2018-01-04 12:46:15 +0100

    lein-release plugin: preparing 0.11.19 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.11.19-SNAPSHOT"
+(defproject de.otto/tesla-microservice "0.11.19"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
