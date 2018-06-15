d3767984f9a97224c6c36bea19c3bf9e624f93d6
Carl Düvel <c.a.duevel@gmail.com>
2018-06-15 16:00:06 +0200

    prepare release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.11.26-SNAPSHOT"
+(defproject de.otto/tesla-microservice "0.12.0-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
