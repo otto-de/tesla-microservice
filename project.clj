94e7c976c222a68997a86065df2bb5fa1887457f
Christian Stamm <christian.stamm@otto.de>
2019-09-26 09:13:20 +0200

    v0.13.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.13.1"
+(defproject de.otto/tesla-microservice "0.13.2-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
