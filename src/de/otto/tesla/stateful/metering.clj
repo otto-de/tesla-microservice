7dee051f3cccd963f4fbea7e7bb03726c46be7e4
Christian Stamm <christian.stamm@otto.de>
2020-03-23 17:35:18 +0100

    fix metering auth. auth will be cleaned up later

--- src/de/otto/tesla/stateful/metering.clj
+++ src/de/otto/tesla/stateful/metering.clj
@@ -7,11 +7,10 @@
     [de.otto.tesla.middleware.auth :as auth]
     [de.otto.tesla.stateful.handler :as handlers]
     [compojure.core :as c]
-    [overtone.at-at :as at]
-    [de.otto.tesla.stateful.scheduler :as sched]))
+    [overtone.at-at :as at]))
 
 (defn write-to-console []
-  (log/info "Metrics Reporting:\n"  (goo/text-format)))
+  (log/info "Metrics Reporting:\n" (goo/text-format)))
 
 (defn start-console-reporter [console-config scheduler]
   (let [interval-in-ms (* 1000 (:interval-in-s console-config))]
@@ -26,32 +25,32 @@
 (defn- path-filter [metrics-path handler]
   (c/GET metrics-path request (handler request)))
 
-(defn register-metrics-endpoint [{metrics-path :metrics-path :as config} handler authenticate-type authenticate-fn]
+(defn register-metrics-endpoint [{metrics-path :metrics-path} {:keys [config handler authenticate-type prometheus-auth-fn]}]
   (log/info "Register metrics prometheus endpoint")
   (handlers/register-handler handler ((->> metrics-response
                                            (goo/timing-middleware)
-                                           (auth/wrap-auth authenticate-type authenticate-fn config)
+                                           (auth/wrap-auth authenticate-type prometheus-auth-fn (:config config))
                                            (partial path-filter metrics-path)))))
 
-(defn- start-reporter! [handler scheduler authenticate-type prometheus-auth-fn [reporter-type reporter-config]]
+(defn- start-reporter! [{:keys [scheduler] :as self} [reporter-type reporter-config]]
   (case reporter-type
     :console (start-console-reporter reporter-config scheduler)
-    :prometheus (register-metrics-endpoint reporter-config handler authenticate-type prometheus-auth-fn)))
+    :prometheus (register-metrics-endpoint reporter-config self)))
 
-(defn- start-reporters! [config handler scheduler authenticate-type prometheus-auth-fn]
+(defn- start-reporters! [{:keys [config] :as self}]
   (let [available-reporters (get-in config [:config :metrics])]
-    (run! (partial start-reporter! handler scheduler authenticate-type prometheus-auth-fn) available-reporters)))
+    (run! (partial start-reporter! self) available-reporters)))
 
 (defrecord Metering [authenticate-type prometheus-auth-fn config handler scheduler]
   component/Lifecycle
   (start [self]
     (log/info "-> starting metering.")
     (goo/register-counter! :metering/errors {:labels [:error :metric-name]})
-    (assoc self :reporters (start-reporters! config handler scheduler authenticate-type prometheus-auth-fn)))
+    (assoc self :reporters (start-reporters! self)))
 
   (stop [self]
     (log/info "<- stopping metering")
-     self))
+    self))
 
 (defn new-metering
   ([]
