5fed4783a46e9c1d7759a586bda887ae7be29408
Daniel Rolfes <daniel.rolfes@otto.de>
2020-03-30 15:00:31 +0200

    version 0.15.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.14.1"
+(defproject de.otto/tesla-microservice "0.15.0"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
