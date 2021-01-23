ef629d227e90c611d79eb53d2f76b31fd4ba0155
Carl Düvel <c.a.duevel@gmail.com>
2021-01-23 14:20:45 +0100

    Prepare 0.16.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.15.2"
+(defproject de.otto/tesla-microservice "0.16.0"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
