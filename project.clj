9ee29134a7d852b27448223408a50480d32db1a3
Carl Düvel <c.a.duevel@gmail.com>
2021-01-23 14:11:32 +0100

    Deps update

--- project.clj
+++ project.clj
@@ -5,20 +5,20 @@
             :url  "http://www.apache.org/license/LICENSE-2.0.html"}
   :scm {:name "git"
         :url  "https://github.com/otto-de/tesla-microservice"}
-  :dependencies [[org.clojure/data.json "0.2.7"]
-                 [org.clojure/tools.logging "0.5.0"]
+  :dependencies [[org.clojure/data.json "1.0.0"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [de.otto/status "0.1.3"]
                  [de.otto/goo "1.2.12"]
                  [beckon "0.1.1"]
                  [clj-time "0.15.2"]
                  [clojurewerkz/propertied "1.3.0"]
-                 [com.stuartsierra/component "0.4.0"]
-                 [compojure "1.6.1"]
-                 [environ "1.1.0"]
+                 [com.stuartsierra/component "1.0.0"]
+                 [compojure "1.6.2"]
+                 [environ "1.2.0"]
                  [overtone/at-at "1.2.0"]
-                 [ring/ring-core "1.8.0"]
-                 [ring/ring-devel "1.8.0"]
-                 [ring-basic-authentication "1.0.5"]]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-devel "1.8.2"]
+                 [ring-basic-authentication "1.1.0"]]
 
   :exclusions [org.clojure/clojure
                org.slf4j/slf4j-nop
@@ -38,6 +38,7 @@
                                       [org.slf4j/slf4j-api "1.7.30"]
                                       [ch.qos.logback/logback-core "1.2.3"]
                                       [ch.qos.logback/logback-classic "1.2.3"]
-                                      [ring-mock "0.1.5"]]
+                                      [ring-mock "0.1.5"]
+                                      [org.clojure/data.codec "0.1.1"]]
                        :plugins      [[lein-release/lein-release "1.0.9"]]}}
   :test-paths ["test" "test-resources" "test-utils"])
