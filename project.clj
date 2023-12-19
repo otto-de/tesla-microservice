f5dc5a4476d4fa641ba3d4a374088af321b0b37c
Daniel Rolfes <daniel.rolfes@otto.de>
2023-12-19 16:01:38 +0100

    lein-release plugin: bumped version from 0.17.5 to 0.17.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-microservice "0.17.5"
+(defproject de.otto/tesla-microservice "0.17.6-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-microservice"
   :license {:name "Apache License 2.0"
